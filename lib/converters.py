6647578bfe2a29e64dbed61ec7f44de47e1e9fad
Jarosław Kilian <jaros1024@gmail.com>
2018-05-27 21:16:00 +0200

    Created nanowattToBeats converter

--- lib/converters.py
+++ lib/converters.py
@@ -1,8 +1,41 @@
 import pandas
+import math
 
 
 def ohmToMicroSiemens(ohm):
     return (1/ohm)*1000000.0
+
+
+def nanowattToBeats(inputFile, outputFile):
+    heart = pandas.read_csv(inputFile, sep=';', dtype={"timestamp": int, "value": float})
+
+    results = []
+
+    lastTimestamp = 0
+    lastValue = 0
+    lastBpm = 0
+    rising = True
+
+    for i in heart.values:
+        if i[1] > -5:
+            continue
+
+        if lastTimestamp == 0:
+            lastTimestamp = i[0]
+            lastValue = i[1]
+        else:
+            if lastValue > i[1]:
+                rising = False
+            elif lastValue < i[1]:
+                if not rising:
+                    bpmValue = __intervalToBpmSec(i[0]-lastTimestamp)
+                    if (math.fabs(bpmValue - lastBpm) <= 30 or lastBpm == 0) and (50 < bpmValue < 150):
+                        results.append((round(i[0]*1000), bpmValue))
+                    lastTimestamp = i[0]
+                rising = True
+
+    __saveToFile(outputFile, results)
+    return
 
 
 def microvoltToBeats(inputFile, outputFile):
@@ -15,7 +48,9 @@ def microvoltToBeats(inputFile, outputFile):
     for i in heart.values:
         if i[1] >= 800 and not isBeat:
             if lastBeat != 0:
-                results.append((i[0], __intervalToBpm(i[0]-lastBeat)))
+                bpm = __intervalToBpm(i[0]-lastBeat)
+                if bpm < 200:
+                    results.append((i[0], bpm))
             lastBeat = i[0]
             isBeat = True
         if i[1] <= 800 and isBeat:
@@ -27,6 +62,9 @@ def microvoltToBeats(inputFile, outputFile):
 
 def __intervalToBpm(interval):
     return 60000.0/interval
+
+def __intervalToBpmSec(interval):
+    return 60.0/interval
 
 
 def __saveToFile(path, values):
