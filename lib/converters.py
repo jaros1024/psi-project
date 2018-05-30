2d3ed78a1c08f6440ec000a11a6e0cf8d3d5404c
Jarosław Kilian <jaros1024@gmail.com>
2018-05-30 12:52:32 +0200

    Improved Empatica analysis

--- lib/converters.py
+++ lib/converters.py
@@ -1,5 +1,4 @@
 import pandas
-import math
 
 
 def ohm_to_microsiemens(ohm):
@@ -9,32 +8,11 @@ def ohm_to_microsiemens(ohm):
 def nanowatt_to_beats(input_file, output_file):
     heart = pandas.read_csv(input_file, sep=';', dtype={"timestamp": int, "value": float})
 
-    results = []
+    extremes = __get_local_extremes(heart.values)
+    diastolic_points = __get_diastolic_points(extremes)
+    bpm = __dialistic_points_to_beats(diastolic_points)
 
-    last_timestamp = 0
-    last_value = 0
-    last_bpm = 0
-    rising = True
-
-    for i in heart.values:
-        if i[1] > -5:
-            continue
-
-        if last_timestamp == 0:
-            last_timestamp = i[0]
-            last_value = i[1]
-        else:
-            if last_value > i[1]:
-                rising = False
-            elif last_value < i[1]:
-                if not rising:
-                    bpm_value = __interval_to_bpm_sec(i[0] - last_timestamp)
-                    if (math.fabs(bpm_value - last_bpm) <= 30 or last_bpm == 0) and (50 < bpm_value < 150):
-                        results.append((round(i[0]*1000), bpm_value))
-                    last_timestamp = i[0]
-                rising = True
-
-    __save_to_file(output_file, results)
+    __save_to_file(output_file, bpm)
     return
 
 
@@ -75,3 +53,54 @@ def __save_to_file(path, values):
             file.write(f"{i[0]}, {round(i[1], 2)}\n")
     return
 
+
+def __get_local_extremes(data):
+    results = []
+
+    for i in range(1, (len(data)-1)):
+        # check for local minimum
+        if data[i-1][1] > data[i][1] and data[i+1][1] > data[i][1]:
+            results.append((data[i][0], data[i][1], "min"))
+        if data[i - 1][1] < data[i][1] and data[i + 1][1] < data[i][1]:
+            results.append((data[i][0], data[i][1], "max"))
+
+    return results
+
+
+def __get_diastolic_points(extremes):
+    diastolic_points = []
+
+    minima = []
+    for i in extremes:
+        if i[2] == "min":
+            if i[1] < -2:
+                minima.append(i)
+        else:
+            if i[1] > 0:
+                point = __get_lowest_min(minima)
+                if point is not None:
+                    diastolic_points.append(point)
+                minima.clear()
+
+    return diastolic_points
+
+def __get_lowest_min(minima):
+    lowest = (0, 999)
+
+    for i in minima:
+        if i[1] < lowest[1]:
+            lowest = (i[0], i[1])
+
+    if lowest[1] < 0:
+        return lowest
+    return None
+
+
+def __dialistic_points_to_beats(points):
+    results = []
+    for i in range(1, len(points)):
+        bpm_value = __interval_to_bpm_sec(points[i][0]-points[i-1][0])
+        if 50 < bpm_value < 130:
+            results.append((round(points[i][0]*1000), round(bpm_value, 2)))
+
+    return results
