1dcc5923c7355da7a7d6c14dc0e5d9841e963a76
Matshec <mmasakra@gmail.com>
2018-06-01 00:09:55 +0200

    added gsr data mean

--- lib/processors.py
+++ lib/processors.py
@@ -16,9 +16,10 @@ def extract_data_for_single_image(path, time_range=10):
     images_path = os.path.join(inner_root_path,'images.csv')
 
     images_data = pandas.read_csv(images_path, sep=',', header=None).values
-    my_dtype = {'value': np.object}
-    bmp_data = pandas.read_csv(bmp_filepath, sep=',',dtype=my_dtype, parse_dates=[1]).values
-    gsr_data = pandas.read_csv(gsr_filepath, sep=',', dtype=my_dtype,parse_dates=[1]).values
+    my_dtype = {'timestamp':np.uint64,'value': float}
+    bmp_data = pandas.read_csv(bmp_filepath, sep=',',dtype=my_dtype).values
+    gsr_data = pandas.read_csv(gsr_filepath, sep=',', dtype=my_dtype).values
+    gsr_data = _mean_data(gsr_data, 5)
     for index, image in enumerate(images_data):
         name = _get_filename(image[0])
         start_time = image[1]
@@ -30,6 +31,7 @@ def extract_data_for_single_image(path, time_range=10):
         filtered_gsr = _find_data_in_range(gsr_data,start_time, time_range, next_start_time)
         output[name] = (filtered_bpm, filtered_gsr)
     return output
+
 
 
 
@@ -50,3 +52,14 @@ def _find_data_in_range(raw_data: np.ndarray, start:int, range_: int, max_time:i
     return found
 
 
+def _mean_data(data:[], window_size:int) -> []:
+    total_len = len(data)
+    index = 0
+    out = []
+    while index + window_size < total_len:
+        chunk = data[index : index + window_size]
+        middle_time = data[(2*index + window_size) // 2][0]
+        mean = np.mean(chunk[:,1])
+        out.append(np.asarray([middle_time, mean]))
+        index = index + window_size
+    return out
