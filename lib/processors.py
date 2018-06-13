5d2392d627efa0ab2f10863eb581213759bee30d
Matshec <mmasakra@gmail.com>
2018-06-13 15:50:13 +0200

    docstrings and code cleaning

--- lib/processors.py
+++ lib/processors.py
@@ -1,54 +1,14 @@
-import csv
 import pandas
 import os
 import numpy as np
 
 SAMPLE_ROOT = '2018-afcai-spring'
-# output_root='2018-asia-winter'
-
 NAPS_VALIDATION_DATA = pandas.read_csv('NAPS_valence_arousal_2014.csv', sep=';', names=['ID', 'Category', 'Nr', 'V_H',
                                                                                         'Description', 'Valence',
                                                                                         'Arousal'])
-
-
-def gather_info():
-    cleanOutput()
-    dirs = next(os.walk(SAMPLE_ROOT))[1]
-    for directory in dirs:
-        current_dir = os.path.join(SAMPLE_ROOT, directory)
-        data = extract_data_for_single_image(current_dir)
-        for image in data:
-            handle_image(os.path.join(image, directory), data.get(image), image)
-        break
-
-
-def handle_image(path: str, data: tuple, im):
-    image_file = os.path.join(output_root, path) + '.csv'
-    image_dir = os.path.join(output_root, im)
-    os.mkdir(image_dir)
-
-    # Trzeba wyciagnac z daty wartosci i wpisac je do pliku
-
-    with open(image_file, 'w', ) as image_csv:
-        writer = csv.writer(image_csv, delimiter=';', escapechar=' ', quotechar='', quoting=csv.QUOTE_NONE)
-        bpm, gsr = data
-        # write bpm
-        # for value in bmp:
-        #
-        #     image_csv.close()
-
-
-def cleanOutput():
-    for root, dirs, files in os.walk(output_root, topdown=False):
-        for name in files:
-            os.remove(os.path.join(root, name))
-        for name in dirs:
-            os.rmdir(os.path.join(root, name))
-
-
 def merge_bpm_and_csr(d_root):
     """"
-    merges bpm and csr into single list of values, excluding timestamp, adds info about what prediceted valued shoulld be
+    loads all processed data to pandas.DataFrame
     :returns pandas DataFrame with rows columns [bpm. gsr ,valence ,arousal]
     """
     final_list = pandas.DataFrame(columns=['bpm', 'gsr', 'valence', 'arousal'])
@@ -69,6 +29,7 @@ def extract_data_for_single_image(path, time_range=10, keep_timestamp=True):
     functions assumes that data is converted and 'output' folder in given path exists
     :param path: path to single person folder
     :param time_range: time range which will be extracted in seconds
+    :param keep_timestamp: optional swtich to return data lists with or without timestamp
     :returns dictionary with picture names as keys and tuple of data arrays as data ([bpm],[gsr])"""
     output = {}
     inner_root_path = os.path.join(path, 'output')
@@ -94,12 +55,27 @@ def extract_data_for_single_image(path, time_range=10, keep_timestamp=True):
 
 
 def _get_filename(base_str: str) -> str:
+    """
+    parses out filename from path
+    :param base_str: string with unix like path and filename at the end
+    :return: name of the file
+    """
     splited = base_str.split('\\')
     name = splited[-1]
     return name.split('.')[0]
 
 
 def _find_data_in_range(raw_data: np.ndarray, start: int, range_: int, max_time: int, skip_timestamp=False) -> []:
+    """
+    finds bpm or gsr data from given time renge, startgin from cleset following record  from when picture was shown
+    through given range or to start of new timestamp when  next picture was shown
+    :param raw_data: raw data to search
+    :param start: picture show time
+    :param range_: range to exctract
+    :param max_time: max possible range
+    :param skip_timestamp: optional if given data without timestamp is returned
+    :return:  list of exctracred data with timstamps
+    """
     ns_range = range_ * 1000
     if start + ns_range > max_time:
         ns_range = max_time - start
@@ -107,6 +83,7 @@ def _find_data_in_range(raw_data: np.ndarray, start: int, range_: int, max_time:
     found = [x[1] for x in raw_data if 0 <= x[0] - start <= ns_range] if skip_timestamp else \
         [x for x in raw_data if 0 <= x[0] - start <= ns_range]
     return found
+
 
 
 def _find_v_a(pandas_df, name):
